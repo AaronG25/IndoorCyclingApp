258ca4bc28e1cefe394389554e35e1cefdf7418d
AaronG25 <aaron.hog@laposte.net>
2026-01-17 18:07:36 +0100

    Update BleConstants.kt

--- app/src/main/java/com/indoorcycling/app/ble/BleConstants.kt
+++ app/src/main/java/com/indoorcycling/app/ble/BleConstants.kt
@@ -3,12 +3,6 @@ package com.indoorcycling.app.ble
 import java.util.UUID
 
 object BleConstants {
-
-    // Cycling Speed and Cadence Service
-    val CSC_SERVICE_UUID: UUID =
+    val CYCLING_CADENCE_SERVICE: UUID =
         UUID.fromString("00001816-0000-1000-8000-00805f9b34fb")
-
-    // CSC Measurement characteristic
-    val CSC_MEASUREMENT_UUID: UUID =
-        UUID.fromString("00002a5b-0000-1000-8000-00805f9b34fb")
 }
